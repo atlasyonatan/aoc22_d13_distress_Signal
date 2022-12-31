d858aa5ec3a45f91b703da526e9cf11ff89d13b1
Yonatan Atlas <atlasyonatan@gmail.com>
2022-12-31 04:15:37 +0200

    changed tree parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 use std::{
     fs::File,
     io::{self, BufRead},
-    mem::discriminant,
     path::Path,
-    str::FromStr,
 };
-
-use itertools::{Chunk, Itertools};
-use lazy_static::lazy_static;
-use regex::Regex;
+use std::{cmp::Ordering, str::FromStr};
+use itertools::Itertools;
 
 fn main() {
     let path = Path::new("../input.txt");
@@ -21,13 +17,13 @@ fn main() {
         .into_iter()
         .map(|chunk| {
             let chunk: [String; 2] = chunk.take(2).collect_vec().try_into().unwrap();
-            chunk.map(|l| Node::<u8>::from_str(l.as_str()).unwrap())
+            chunk.map(|l| Node::<i32>::from_str(l.as_str()).unwrap())
         })
         .enumerate()
         .filter_map(
             |(i, signals)| match signals_are_ordered(&signals[0], &signals[1]) {
-                true => Some(i),
-                false => None,
+                Ordering::Less => Some(i),
+                _ => None,
             },
         )
         .sum::<usize>();
@@ -43,33 +39,71 @@ impl<T: FromStr> FromStr for Node<T> {
     type Err = T::Err;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        lazy_static! {
-            static ref RE: Regex = Regex::new(r"^\[(.*)\]$").unwrap();
-        }
-        Ok(match RE.captures(s) {
-            Some(cap) => {
-                let mat = cap.get(1).unwrap();
-                let mut v = Vec::new();
-                for s in mat.as_str().split(',') {
-                    v.push(Node::from_str(s)?);
+        let mut stack: Vec<Node<T>> = Vec::new();
+        let mut current_string: Option<String> = None;
+        for char in s.chars() {
+            match char {
+                '[' => {
+                    stack.push(Node::List(Vec::new()));
                 }
-                Node::List(v)
+                ']' => {
+                    if stack.len() > 1 {
+                        let node = stack.pop().unwrap();
+                        if let Node::List(list) = stack.last_mut().unwrap() {
+                            list.push(node);
+                        }
+                    } else if stack.len() == 1 {
+                        return Ok(stack.pop().unwrap());
+                    }
+                }
+                ',' => (),
+                other => {
+                    if let None = current_string {
+                        current_string = Some(String::new())
+                    }
+                    if let Some(string) = current_string.as_mut() {
+                        string.push(other);
+                    }
+                    continue;
+                }
             }
-            None => Node::Single(T::from_str(s)?),
-        })
+            if let Some(string) = current_string {
+                let t = T::from_str(string.trim())?;
+                if let Node::List(list) = stack.last_mut().unwrap() {
+                    list.push(Node::Single(t));
+                }
+            }
+            current_string = None;
+        }
+        panic!("Invalid string")
     }
 }
 
-fn signals_are_ordered<T: PartialOrd>(first: &Node<T>, second: &Node<T>) -> bool {
-    
-    let (first, second) = match first {
-        Node::Single(first_single) => match second {
-            Node::Single(second_single) => return first_single <= second_single,
-            Node::List(_) => (, second),
+fn signals_are_ordered<T: Ord + Copy>(left: &Node<T>, right: &Node<T>) -> Ordering {
+    match left {
+        Node::Single(left_single) => match right {
+            Node::Single(right_single) => left_single.cmp(right_single),
+            Node::List(_) => {
+                signals_are_ordered(&Node::List(vec![Node::Single(*left_single)]), right)
+            }
         },
-        Node::List(first) => match second {
-            Node::Single(second) => (first, vec![second]),
-            Node::List(second) => (first, second),
+        Node::List(left_list) => match right {
+            Node::Single(right_single) => {
+                signals_are_ordered(left, &Node::List(vec![Node::Single(*right_single)]))
+            }
+            Node::List(right_list) => {
+                for i in 0..left_list.len().min(right_list.len()) {
+                    match signals_are_ordered(&left_list[i], &right_list[i]) {
+                        Ordering::Equal => {
+                            continue;
+                        }
+                        ordering => {
+                            return ordering;
+                        }
+                    }
+                }
+                left_list.len().cmp(&right_list.len())
+            }
         },
     }
 }
